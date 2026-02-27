b655b160fbbf2ba152524bd7dad473e65d012321
henriant <henant@outlook.com>
2026-02-27 14:01:53 +0100

    Oppsett grunnleggende API-funksjon via forelesning
    Opprettet API-tilkobling og søkefunksjon for innholdet i denne

---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+export default function Home(){
+
+    const [search, setSearch] = useState()
+
+    const baseUrl = `http://www.omdbapi.com/?s=${search}&apikey=`
+    // IKKE GJØR DETTE: !!!
+    const apiKey = '4056d565'
+
+    const getMovies = async()=> {
+        try
+        {
+            const response = await fetch(`${baseUrl}${apiKey}`)
+            const data = await response.json()
+            console.log(data)
+        }
+        catch(err){
+            console.error(err);
+        }
+    }
+
+    const handleChange = (e) => {
+        setSearch(e.target.value)
+    }
+
+    return(
+    <main>
+        <h1>Forside</h1>
+        <form>
+            <label>
+                Søk etter film
+                <input type="search" placeholder="James Bond" onChange={handleChange}></input>
+            </label>
+        </form>
+        <button onClick={getMovies}>Søk</button>
+    </main>
+    )
+}
